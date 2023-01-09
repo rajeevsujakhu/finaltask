ed60a1886be80c6831d2e3c8c29655e482bee710
Rajeev Sujakhu <rajeevsujakhau@gmail.com>
2023-01-09 20:12:23 +0545

    created footer part

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
+var owl = $('.owl-carousel');
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         items: 1,
         loop: true,
         nav: true,
         autoplay:true,
+        autoplayTimeout:3000,
     });
   });
+    $('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+    })
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[3000])
+    })
+    
+    $(document).ready(function () {
+        $('.stop').click(function () {
+            $('.stop i').toggleClass('fa-light fa-play');
+            $(this).toggleClass('play');
+        });
+    });
